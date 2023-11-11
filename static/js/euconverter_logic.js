1005252ee45f2a60dc7ca0261d1c076f7066657e
OmarMesqq <omarmsqt@gmail.com>
2023-11-11 00:36:02 -0300

    feat: :sparkles: add nm in euconverter

--- static/js/euconverter_logic.js
+++ static/js/euconverter_logic.js
@@ -4,11 +4,19 @@ const unitConversions = {
     kJ_per_mol: 0.0003808798033989866, // 1 Hartree = 2625.5 kJ/mol
     kcal_per_mol: 0.0015936010974213599, // 1 Hartree = 627.5095 kcal/mol
     cm_minus1: 0.0000045563352812122295, // 1 Hartree = 219474.63 cm^-1
-    nm_minus1: 45.5633528121223 // 1 Hartree = 0.021947463 nm^-1
+    nm: (hartreeValue) => { 
+        const eV_per_hartree = 27.2114; 
+        return 1239.8 / (hartreeValue * eV_per_hartree);
+    }
 }
 
 const energyConverter = (value, sourceUnit, targetUnit) => {
     if (!isNaN(value)) {
+        // Check if the target unit is nm and source is Hartrees
+        if (targetUnit === 'nm' && sourceUnit === 'Hartrees') {
+            return unitConversions.nm(value).toPrecision(10);
+        };
+
         return parseValue(
             (value * (unitConversions[sourceUnit] / unitConversions[targetUnit]))
             .toPrecision(10)
