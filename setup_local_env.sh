e2c7e1944126b5d20157e98e2732cb34c90f7d24
OmarMesqq <omarmsqt@gmail.com>
2023-12-10 16:15:20 -0300

    chore: create a single local env setup script

--- setup_local_env.sh
+++ setup_local_env.sh
@@ -14,3 +14,8 @@ mkdir -p ./app/upload/plqsim/input_files
 mkdir -p ./app/upload/plqsim/result
 mkdir -p ./app/upload/tmmsim/input_files
 mkdir -p ./app/upload/tmmsim/result
+
+cp mocks/fret_calc_mock.py fret_calc.py
+cp mocks/plq_sim_mock.py plq_sim.py
+cp mocks/ri_calc_mock.py ri_calc.py
+cp mocks/tmm_sim_mock.py tmm_sim.py
--- setup_mocks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp mocks/fret_calc_mock.py fret_calc.py
-cp mocks/plq_sim_mock.py plq_sim.py
-cp mocks/ri_calc_mock.py ri_calc.py
-cp mocks/tmm_sim_mock.py tmm_sim.py
