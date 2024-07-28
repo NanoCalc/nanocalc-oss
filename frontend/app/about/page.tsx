d77b70abf7483b8e220a90bac0090ddd6a90f598
OmarMesqq <omarmsqt@gmail.com>
2024-07-27 21:06:23 -0300

    refactor: :recycle: create Contributor component for better reusability

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,71 +1,44 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Contributor from '../components/Contributor';
+
+
+const contributors = [
+    {
+        imagePath: "/graziani.jpg",
+        name: "Graziani Candiotto",
+        text: "has a BSc, MSc and PhD in Physics by UFSC. He is currently a researcher at UFRJ's Physics Department and a member of the NAMOR (Nanoscience Modelling in Rio) group. Has experience in Condensed Matter Physics, Statistical and Quantum Mechanics, as well as in Machine Learning and Materials Science. He also develops high performance software that describes OFEDs and pharmacokinetics."
+    },
+    {
+        imagePath: "/leandro.jpg",
+        name: "Leandro Benatto",
+        text: "holds a Bachelor's, Master's, and PhD degree in Physics from UFPR. Currently, he is a FAPERJ Postdoctoral Fellow who is primarily focused on the study of new organic materials for use in optoelectronic devices. With extensive knowledge in Python programming, he has also developed software for analyzing lab results and simulating experiments. With his expertise in both physics and programming, Leandro is well-equipped to tackle complex research problems in the field of optoelectronics."
+    },
+    {
+        imagePath: "/omar.jpg",
+        name: "Omar Mesquita",
+        text: "is the developer and administrator of the Nanocalc project website. He currently studies Computer Science at PUC-RIO. You can contact him at his my email address or social networks :)"
+    },
+    {
+        imagePath: "/jose.jpeg",
+        name: "José Furtado",
+        text: "studies Chemical Engineering at UFRJ and is currently working as a junior researcher at the Physics Department, where he learns programming to eventually be able to do more complex tasks in the research group."
+    }
+];
+
+
+
 
 export default function About() {
-return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-         <section className="w-full h-screen flex flex-col items-center">
-         <Image
-		src="/graziani.jpg"
-		width={200}
-		height={200}
-		alt="Picture of Graziâni Candiotto"
-		className="mt-8 mb-4"
-	  />
-	<h2 className="ml-4 mr-4 mt-10 md:w-1/2 justify-text">
-		<b>Graziâni Candiotto</b> has a BSc, MSc and PhD in Physics by UFSC. He is currently a researcher at UFRJ's Physics Department and a member
-		of the NAMOR (Nanoscience Modelling in Rio) group. Has experience in Condensed Matter Physics, Statistical 
-		and Quantum Mechanics, as well as in Machine Learning and Materials Science. 
-		He also develops high performance software that describes OFEDs and pharmacokinetics.
-	</h2>
-        </section>
-       <section className="w-full h-screen flex flex-col items-center">
-      	<Image
-		src="/leandro.jpg"
-		width={200}
-		height={200}
-		alt="Picture of Leandro Benatto"
-		className="mt-8 mb-4"
-	  />
-	<h2 className="ml-4 mr-4 mt-10 md:w-1/2 justify-text">
-		<b>Leandro Benatto</b> holds a Bachelor's, Master's, and PhD degree in Physics from UFPR. Currently, he is a FAPERJ Postdoctoral 
-		Fellow who is primarily focused on the study of new organic materials for use in optoelectronic devices. 
-		With extensive knowledge in Python programming, he has also developed software for analyzing lab results 
-		and simulating experiments. With his expertise in both physics and programming, Leandro is well-equipped 
-		to tackle complex research problems in the field of optoelectronics. 
-	</h2>
- 
-       </section>
-	<section className="w-full h-screen flex flex-col items-center">
-	<Image
-		src="/omar.jpg"
-		width={200}
-		height={200}
-		alt="Picture of Omar Mesquita"
-		className="mt-8 mb-4"
-	  />
-	<h2 className="ml-4 mr-4 mt-10 md:w-1/2 justify-text">
-		Hi! My name is Omar Mesquita and I am the developer and administrator of this website and its web applications.
-		I am currently studying Computer Science at PUC-RIO.
-		Feel free to contact me at my email or social networks :) 
-	</h2>
-
-	</section>
-       <section className="w-full h-screen flex flex-col items-center">
-       <Image
-		src="/jose.jpeg"
-		width={200}
-		height={200}
-		alt="Picture of José Furtado"
-		className="mt-8 mb-4"
-	  />
-	<h2 className="ml-4 mr-4 mt-10 md:w-1/2 justify-text">
-		Hello! My name is José Furtado and I am studying Chemical Engineering at UFRJ.
-		I am currently working as a junior researcher at the Physics Department, where I'm studying programming 
-		to eventually be able to do more complex tasks in our research.
-	</h2>
-       </section>
-    </main>
-  );
-
-
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between">
+            {contributors.map((contributor, index) => (
+                <Contributor 
+                    key={index}
+                    imagePath={contributor.imagePath}
+                    name={contributor.name}
+                    text={contributor.text}
+                />
+            ))}
+        </main>
+    );
 }
--- frontend/app/components/Contributor.tsx
+++ frontend/app/components/Contributor.tsx
@@ -1,11 +1,18 @@
+import Image from 'next/image'
 
-
-export default function Contributor() {
-	return (
-	<section className="bg-blue-500 w-full h-screen flex items-center justify-center">
-	
-	</section>
-
-	)
-
+export default function Contributor({ imagePath, name, text }) {
+    return (
+        <section className="w-full h-screen flex flex-col items-center">
+            <Image
+                src={imagePath}
+                width={200}
+                height={200}
+                alt={`Picture of ${name}`}
+                className="mt-8 mb-4"
+            />
+            <h2 className="ml-4 mr-4 mt-10 md:w-1/2 text-justify">
+                <b>{name}</b> {text}
+            </h2>
+        </section>
+    );
 }
