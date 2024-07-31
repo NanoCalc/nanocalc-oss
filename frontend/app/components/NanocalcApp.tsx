0691e8ffc1d952a2c16c298bd724d396b8feb7b4
OmarMesqq <omarmsqt@gmail.com>
2024-07-30 23:08:15 -0300

    refactor: :recycle: decouple common logos display logic from main nanocalc app component

--- frontend/app/components/NanocalcApp.tsx
+++ frontend/app/components/NanocalcApp.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import CommonLogos from './CommonLogos';
+import commonLogos from '../lib/common_logos';
 
 export default function NanocalcApp({ appLogoPath, appName }) {
 	return (
@@ -17,43 +19,7 @@ export default function NanocalcApp({ appLogoPath, appName }) {
 					className="mt-4 rounded-lg  w-auto h-auto"
 				/>
 
-				<Image
-					src="/common_logos/dine.png"
-					width={195}
-					height={97}
-					alt="DINE logo - Grupo de Dispositivos Nanoestruturados"
-					priority={true}
-					className="mt-4 dark:bg-white rounded-lg"
-				/>
-
-				<Image
-					src="/common_logos/namor.png"
-					width={195}
-					height={83}
-					alt="NAMOR logo - Nanoscience Modelling in Rio"
-					priority={true}
-					className="mt-4 dark:bg-white  rounded-lg"
-				/>
-				<figure aria-labelledby="Logos of associated universities with the project" className="flex flex-row">
-					<Image
-						src="/common_logos/ufrj.png"
-						width={78}
-						height={103}
-						alt="UFRJ logo - Universidade Federal do Rio de Janeiro"
-						priority={true}
-						className="mt-4 dark:bg-white  rounded-lg mr-2 "
-					/>
-
-					<Image
-						src="/common_logos/ufpr.png"
-						width={111}
-						height={78}
-						alt="UFPR logo- Universidade Federal do Paraná"
-						priority={true}
-						className="mt-4 dark:bg-white  rounded-lg ml-2 "
-					/>
-
-				</figure>
+				<CommonLogos logos={commonLogos}/>
 			</section>
 		</main>
 	);
