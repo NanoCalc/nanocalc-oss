09ad1a5e1f07f6c3e5b6f46bc5865ed8c5a22369
OmarMesqq <omarmsqt@gmail.com>
2024-07-30 22:52:17 -0300

    fix: :bug: fixed compilation error where map over image array triggered SSR exception

--- frontend/app/components/CommonLogos.tsx
+++ frontend/app/components/CommonLogos.tsx
@@ -2,21 +2,22 @@ import Image from 'next/image'
 import { CommonLogoConfig } from '../lib/model/CommonLogoConfig'
 
 
-export default function CommonLogos(logos: CommonLogoConfig[]) {
-    return(
+export default function CommonLogos({ logos }: { logos: CommonLogoConfig[] }) {
+    return (
         <>
-        {logos.map((logo) => {
-            <Image
-            src={logo.appLogoPath}
-            width={logo.width}
-            height={logo.height}
-            alt={logo.description}
-            priority={true}
-            className="mt-4 dark:bg-white rounded-lg"
-            />
-        }
+            {logos.map((logo, index) => (
+                <Image
+                    key={index}
+                    src={logo.appLogoPath}
+                    width={logo.width}
+                    height={logo.height}
+                    alt={logo.description}
+                    priority={true}
+                    className="mt-4 dark:bg-white rounded-lg"
+                />
+            )
 
-        )}
+            )}
         </>
     )
 }
