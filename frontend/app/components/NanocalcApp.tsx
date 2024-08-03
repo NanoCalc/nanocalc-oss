073c6d3648bc440b3916d15bfe9898d0f9e7902e
OmarMesqq <omarmsqt@gmail.com>
2024-08-03 00:46:13 -0300

    refactor: :recycle: use interface props for NanocalcApp to be in reacts idiom
    use FRET-Calc as the first app as example

--- frontend/app/components/NanocalcApp.tsx
+++ frontend/app/components/NanocalcApp.tsx
@@ -1,27 +1,30 @@
 import Image from 'next/image'
+import { NanocalcAppConfig } from '../lib/model/NanocalcAppConfig';
 import CommonLogos from './CommonLogos';
 import commonLogos from '../lib/common_logos';
 
-export default function NanocalcApp({ appLogoPath, appName }) {
-	return (
-		<main className="flex min-h-screen flex-col justify-between items-center">
-			<section className="w-full h-screen flex flex-col items-center">
-
-			</section>
-
-			<section className="w-full h-screen flex flex-col items-center">
-				<Image
-					src={appLogoPath}
-					width={0}
-					height={0}
-					alt={`${appName} logo`}
-					priority={true}
-					className="mt-4 rounded-lg  w-auto h-auto"
-				/>
-
-				<CommonLogos logos={commonLogos}/>
-			</section>
-		</main>
-	);
+interface NanocalcAppProps {
+  config: NanocalcAppConfig;
 }
 
+export default function NanocalcApp({ config }: NanocalcAppProps) {
+  return (
+    <main className="flex min-h-screen flex-col justify-between items-center">
+      <section className="w-full h-screen flex flex-col items-center">
+        
+      </section>
+
+      <section className="w-full h-screen flex flex-col items-center">
+        <Image
+          src={config.appLogoPath}
+          width={200}
+          height={200}
+          alt={`${config.appName} logo`}
+          priority={true}
+          className="mt-4 rounded-lg w-auto h-auto"
+        />
+        <CommonLogos logos={commonLogos}/>
+      </section>
+    </main>
+  );
+}
--- frontend/app/fretcalc/page.tsx
+++ frontend/app/fretcalc/page.tsx
@@ -1,16 +1,17 @@
 import { Metadata } from 'next';
-import NanocalcApp from '../components/NanocalcApp'
+import NanocalcApp from '../components/NanocalcApp';
 import nanocalcApps from '../lib/nanocalc_apps';
+import { NanocalcAppConfig } from '../lib/model/NanocalcAppConfig';
 
 export const metadata: Metadata = {
   title: "FRET-Calc | Nanocalc",
   description: "Förster resonance energy transfer calculator",
 };
 
-const fretcalcConfig = nanocalcApps["FRET-Calc"]
+const fretcalcConfig: NanocalcAppConfig = nanocalcApps["FRET-Calc"];
 
 export default function Fretcalc() {
   return (
-    <NanocalcApp appLogoPath={fretcalcConfig.appLogoPath} appName={fretcalcConfig.appName} />
+    <NanocalcApp config={fretcalcConfig} />
   );
 }
