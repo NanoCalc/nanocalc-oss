6e6ec6d45cd9b939dd59c7cb6af0685d2ac1476a
OmarMesqq <omarmsqt@gmail.com>
2024-08-03 00:25:03 -0300

    feat: :sparkles: start creating buttons and their modelling

---
+++ frontend/app/lib/model/NanocalcAppConfig.ts
@@ -0,0 +1,27 @@
+interface BaseAppConfig {
+    appLogoPath: string,
+    appName: string,
+    buttons: AppButton[],
+}
+
+interface BaseButton {
+    text: string;
+    allowMultiple?: boolean;
+    isCalculate?: boolean;
+}
+
+interface CalculateButton extends BaseButton {
+    isCalculate: true;
+    expectedExtension?: string;
+}
+
+interface RegularButton extends BaseButton {
+    isCalculate?: false;
+    expectedExtension: string;
+}
+
+type AppButton = CalculateButton | RegularButton;
+
+export interface NanocalcAppConfig {
+    [appName: string]: BaseAppConfig;
+}
--- frontend/app/lib/nanocalc_apps.ts
+++ frontend/app/lib/nanocalc_apps.ts
@@ -1,22 +1,82 @@
+import { NanocalcAppConfig } from "./model/NanocalcAppConfig";
+
 const logosPath = "/nanocalc_apps_logos/";
 
-// Hash table - O(1) lookup
-const nanocalcApps = {
+const nanocalcApps: NanocalcAppConfig = {
     "FRET-Calc": {
         appLogoPath: `${logosPath}fretcalc_logo.svg`,
-        appName: "FRET-Calc"
+        appName: "FRET-Calc",
+        buttons: [
+            {
+                text: "Input file (.xlsx)",
+                expectedExtension: "xlsx",
+            },
+            {
+                text: "Extinction coefficient file (.dat)",
+                expectedExtension: "dat",
+            },
+            {
+                text: "Emission file (.dat)",
+                expectedExtension: "dat",
+            },
+            {
+                text: "Refractive Index file (.dat)",
+                expectedExtension: "dat",
+            },
+            {
+                text: "Click to calculate",
+                isCalculate: true
+            }
+        ]
     },
     "RI-Calc": {
         appLogoPath: `${logosPath}ricalc_logo.svg`,
-        appName: "RI-Calc"
+        appName: "RI-Calc",
+        buttons: [
+            {
+                text: "Input file (.xlsx)",
+                expectedExtension: "xlsx"
+            },
+            {
+                text: "Decadic Abs. Coefficient file (.dat)",
+                expectedExtension: "dat"
+            },
+            {
+                text: " K file (.dat)",
+                expectedExtension: "dat"
+            },
+            {
+                text: "Click to calculate optical constants",
+                isCalculate: true
+            },
+            {
+                text: "Click to calculate n",
+                isCalculate: true
+            }
+        ]
     },
     "PLQ-Sim": {
         appLogoPath: `${logosPath}plqsim_logo.svg`,
-        appName: "PLQ-Sim"
+        appName: "PLQ-Sim",
+        buttons: [
+            {
+                text: "Input file (.xlsx)",
+                expectedExtension: "xlsx"
+            },
+            {
+                text: "Click to calculate Donor Excitation",
+                isCalculate: true
+            },
+            {
+                text: "Click to calculate Acceptor Excitation",
+                isCalculate: true
+            }
+        ]
     },
     "TMM-Sim": {
         appLogoPath: `${logosPath}tmmsim_logo.svg`,
-        appName: "TMM-Sim"
+        appName: "TMM-Sim",
+        buttons: []
     }
 };
 
