71833622ff925212ae171805276e8d7c0a23777a
OmarMesqq <omarmsqt@gmail.com>
2024-08-04 18:21:55 -0300

    refactor: :wheelchair: increase text/bg constrast in article banner

--- frontend/app/components/ArticleBanner.tsx
+++ frontend/app/components/ArticleBanner.tsx
@@ -7,7 +7,7 @@ interface ArticleBannerProps {
 
 export default function ({ config }: ArticleBannerProps) {
     return (
-        <article className="bg-nanocalc-blue rounded-md p-3 text-justify animate-slideIn">
+        <article className="bg-nanocalc-blue rounded-md p-3 text-justify text-white animate-slideIn">
             If you used this tool in your data analysis, please indicate so by citing <b><Link href={config.doi}>{config.title}</Link></b> in your work.
             <br /><br />
             Download some <b><Link href={config.sampleData}>sample data</Link></b>
