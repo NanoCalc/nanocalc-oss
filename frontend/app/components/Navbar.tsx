1800129dfaa645376f095a8982b03c78ce9ca9a3
OmarMesqq <omarmsqt@gmail.com>
2024-08-05 23:35:42 -0300

    refactor: :lipstick: correctly display navbar on desktop/large screens

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
     return (
         <nav ref={navbarRef} className="navbar bg-nanocalc-blue text-white p-4">
             <div className="flex justify-between items-center">
-                <div className="text-xl">Nanocalc</div>
+                <div className="text-xl md:hidden">Nanocalc</div>
                 <div className="md:hidden toggle-button" onClick={handleToggle}>
                     <button>
                         <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
