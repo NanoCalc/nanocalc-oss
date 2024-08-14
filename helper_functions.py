d08c7437da6f06b7e49ca73f5c80b9b515153c4f
OmarMesqq <omarmsqt@gmail.com>
2024-08-13 21:37:48 -0300

    refactor(backend): :recycle: make generate_zip func return the whole path to zip
    also delete some useless funcions

--- helper_functions.py
+++ helper_functions.py
@@ -3,27 +3,21 @@ import uuid
 import zipfile
 import logging
 from config import Config
-from functools import wraps
-from flask import request
-
-
-"""
-Returns True if the file extension is in the list of allowed extensions
-"""
-def allowed_file(filename, allowed_exts):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_exts
+from werkzeug.utils import secure_filename
 
 
 """
 Save a file with a UUID-based name and return its path
 """
-def save_file_with_uuid(directory, file, extension):
-    if not os.path.exists(directory):
-        logging.warning(f"Directory {directory} does not exist. Creating it.")
-        os.makedirs(directory)
-
-    filename = f"{uuid.uuid4()}.{extension}"
-    filepath = os.path.join(directory, filename)
+def save_file_with_uuid(directory, file):
+    secure_name = secure_filename(file.filename)
+    
+    extension = os.path.splitext(secure_name)[1]
+    new_filename = str(uuid.uuid4()) + extension
+    
+    os.makedirs(directory, exist_ok=True)
+    
+    filepath = os.path.join(directory, new_filename)
     file.save(filepath)
     return filepath
 
@@ -34,8 +28,11 @@ Generate a zip file from a directory and return its name
 def generate_zip(path, webapp, upload_folder):
     zip_file_name = f"{uuid.uuid4()}-generated-data.zip"
     zip_file_path = os.path.join(upload_folder, webapp, zip_file_name)
+
     with zipfile.ZipFile(zip_file_path, mode="w") as z:
         for filename in os.listdir(path): 
             file_path = os.path.join(path, filename)
             z.write(file_path, arcname=filename)
-    return zip_file_name
+
+    return zip_file_path
+    
