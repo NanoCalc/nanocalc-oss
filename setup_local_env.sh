e8b373b1aa339fb2ba2b91db379c6f8a3806c608
OmarMesqq <omarmsqt@gmail.com>
2024-08-13 21:38:29 -0300

    chore(backend): :building_construction: change fret and ri to fretcalc and ricalc in setup script

--- setup_local_env.sh
+++ setup_local_env.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 
-mkdir -p ./nanocalc_uploads/fret/emission_files
-mkdir -p ./nanocalc_uploads/fret/extinction_coefficient_files
-mkdir -p ./nanocalc_uploads/fret/index_files
-mkdir -p ./nanocalc_uploads/fret/refractive_index_files
-mkdir -p ./nanocalc_uploads/fret/result
-mkdir -p ./nanocalc_uploads/ri/index_files
-mkdir -p ./nanocalc_uploads/ri/abs_coefficient_files
-mkdir -p ./nanocalc_uploads/ri/k_files
-mkdir -p ./nanocalc_uploads/ri/result
+mkdir -p ./nanocalc_uploads/fretcalc/emission_files
+mkdir -p ./nanocalc_uploads/fretcalc/extinction_coefficient_files
+mkdir -p ./nanocalc_uploads/fretcalc/index_files
+mkdir -p ./nanocalc_uploads/fretcalc/refractive_index_files
+mkdir -p ./nanocalc_uploads/fretcalc/result
+mkdir -p ./nanocalc_uploads/ricalc/index_files
+mkdir -p ./nanocalc_uploads/ricalc/abs_coefficient_files
+mkdir -p ./nanocalc_uploads/ricalc/k_files
+mkdir -p ./nanocalc_uploads/ricalc/result
 mkdir -p ./nanocalc_uploads/plqsim/input_files
 mkdir -p ./nanocalc_uploads/plqsim/result
 mkdir -p ./nanocalc_uploads/tmmsim/input_files
