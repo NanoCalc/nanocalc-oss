4450a91d626eaaab02e17aac9ce505647941d108
OmarMesqq <omarmsqt@gmail.com>
2024-09-01 21:03:01 -0300

    feat(backend): :sparkles: implement plqsim

--- flaskapp.py
+++ flaskapp.py
@@ -84,8 +84,21 @@ def handle_plqsim(files_bundle):
     plqsim_folder = os.path.join(app.config['UPLOAD_FOLDER'], 'plqsim')
 
     try:
-        input_excel_path = save_file_with_uuid(ricalc_folder, files_bundle['inputExcel'])
-        #TODO
+        input_excel_path = save_file_with_uuid(plqsim_folder, files_bundle['inputExcel'])
+        mode = files_bundle['mode']
+
+        if mode == 'donorExcitation':
+            energy_level(input_excel_path, UPLOAD_FOLDER)
+            dataFolderPath = donor_excitation(input_excel_path, UPLOAD_FOLDER)
+        elif mode == 'acceptorExcitation':
+            energy_level(input_excel_path, UPLOAD_FOLDER)
+            dataFolderPath = acceptor_excitation(input_excel_path, UPLOAD_FOLDER)
+        else:
+            #TODO: throw?
+            pass
+        
+        zip_file_name = generate_zip(dataFolderPath, 'plqsim', app.config['UPLOAD_FOLDER'])
+        return zip_file_name
     except Exception as e:
         logging.error(f"handle_plqsim.error: {e}")
         raise e
@@ -108,97 +121,6 @@ app_handlers = {
     'plqsim': handle_plqsim,
     'tmmsim': handle_tmmsim,
 }
-
-# RI Calculator - data upload
-@app.route('/ricalc/submit', methods=['POST'])
-def ri_calc_submit():
-    appName = "RI-Calc"
-    webApp = "ricalc"
-    filesList = []
-
-    try:
-        if not request.files.getlist("kf"):
-            files = request.files.getlist("xif")
-            for file in files:
-                if allowed_file(file.filename,['xlsx']):
-                    xif = save_file_with_uuid(os.path.join(app.config['UPLOAD_FOLDER'], 'ri', 'index_files'), file, 'xlsx')
-                    filesList.append(xif)
-                else:
-                    upload_error = UploadError("file_type", "index file", "xlsx", "ricalc")
-                    return render_template("input_error.html", data=upload_error.to_dict())
-
-            files = request.files.getlist("dacf")
-            for file in files:
-                if allowed_file(file.filename,['dat']):
-                    dacf = save_file_with_uuid(os.path.join(app.config['UPLOAD_FOLDER'], 'ri', 'abs_coefficient_files'), file, 'dat')
-                    filesList.append(dacf)
-                else:
-                    upload_error = UploadError("file_type", "absorption coefficient file", "dat", "ricalc")
-                    return render_template("input_error.html", data=upload_error.to_dict())
-
-            data_n_k = n_k_calculation(filesList[0], filesList[1], UPLOAD_FOLDER)
-            zip_file_name = generate_zip(data_n_k, webApp, Config.UPLOAD_FOLDER)
-            return render_template("upload_success.html", zip_name=zip_file_name,  app_name=appName, webapp=webApp)    
-
-        elif not request.files.getlist("dacf"):
-            files = request.files.getlist("xif")
-            for file in files:
-                if allowed_file(file.filename,['xlsx']):
-                    xif = save_file_with_uuid(os.path.join(app.config['UPLOAD_FOLDER'], 'ri', 'index_files'), file, 'xlsx')
-                    filesList.append(xif)
-                else:
-                    upload_error = UploadError("file_type", "index file", "xlsx", "ricalc")
-                    return render_template("input_error.html", data=upload_error.to_dict())
-
-            files = request.files.getlist("kf")
-            for file in files:
-                if allowed_file(file.filename,['dat']):
-                    kf = save_file_with_uuid(os.path.join(app.config['UPLOAD_FOLDER'], 'ri', 'k_files'), file, 'dat')
-                    filesList.append(kf)
-                else:
-                    upload_error = UploadError("file_type", "K file", "dat", "ricalc")
-                    return render_template("input_error.html", data=upload_error.to_dict())
-
-            data_n = n_calculation(filesList[0], filesList[1], UPLOAD_FOLDER)
-            zip_file_name = generate_zip(data_n, webApp, Config.UPLOAD_FOLDER) 
-            return render_template("upload_success.html", zip_name=zip_file_name,  app_name=appName, webapp=webApp) 
-    except Exception as e:
-        logging.warning(f"Error in RI-Calc: {e}")
-        upload_error = UploadError("file_misformat", None, None, "ricalc")
-        return render_template("input_error.html", data=upload_error.to_dict())
-
-
-# PLQSim view - data upload
-@app.route('/plqsim/submit', methods=['POST'])
-def plq_sim_submit():
-    appName = "PLQSim"
-    webApp = "plqsim"
-    try:
-        index_file = request.files.getlist("xif")
-        for file in index_file:
-            if allowed_file(file.filename,['xlsx']):
-                xif = save_file_with_uuid(os.path.join(app.config['UPLOAD_FOLDER'], 'plqsim', 'input_files'), file, 'xlsx')
-            else:
-                upload_error = UploadError("file_type", "index file", "xlsx", "plqsim")
-                return render_template("input_error.html", data=upload_error.to_dict())
-
-        energy_level(xif, UPLOAD_FOLDER) # Generate Energy Level plot for both choices
-        action = request.form.get('action')
-
-        if action == 'Calculate Acceptor Excitation':
-            data = acceptor_excitation(xif, UPLOAD_FOLDER)
-            zip_file_name = generate_zip(data, webApp, Config.UPLOAD_FOLDER)
-
-        elif action == 'Calculate Donor Excitation':
-            data = donor_excitation(xif,UPLOAD_FOLDER)
-            zip_file_name = generate_zip(data,webApp, Config.UPLOAD_FOLDER)
-
-        return render_template("upload_success.html", zip_name=zip_file_name,  app_name=appName, webapp=webApp)
-
-    except Exception as e:
-        logging.warning(f"Error in PLQSim: {e}")
-        upload_error = UploadError("file_misformat", None, None, "plqsim")
-        return render_template("input_error.html", data=upload_error.to_dict())
 
 
 # TMMSim view - data upload
