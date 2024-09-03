38e335442551a3f00d311e1f6cb8fcc210fceaf4
OmarMesqq <omarmsqt@gmail.com>
2024-09-03 18:11:01 -0300

    chore(backend): :truck: remove useless subfolders for processing

--- backend/setup_local_env.sh
+++ backend/setup_local_env.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
 
-mkdir -p ./nanocalc_uploads/fretcalc/emission_files
-mkdir -p ./nanocalc_uploads/fretcalc/extinction_coefficient_files
-mkdir -p ./nanocalc_uploads/fretcalc/index_files
-mkdir -p ./nanocalc_uploads/fretcalc/refractive_index_files
 mkdir -p ./nanocalc_uploads/fretcalc/result
-mkdir -p ./nanocalc_uploads/ricalc/index_files
-mkdir -p ./nanocalc_uploads/ricalc/abs_coefficient_files
-mkdir -p ./nanocalc_uploads/ricalc/k_files
 mkdir -p ./nanocalc_uploads/ricalc/result
-mkdir -p ./nanocalc_uploads/plqsim/input_files
 mkdir -p ./nanocalc_uploads/plqsim/result
-mkdir -p ./nanocalc_uploads/tmmsim/input_files
 mkdir -p ./nanocalc_uploads/tmmsim/result
