21b34e64ee63c0c5e5a97982575b3a01125d2204
OmarMesqq <omarmsqt@gmail.com>
2024-09-04 13:08:42 -0300

    feat(backend): :sparkles: setup redirect for common routes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/fret',
+                destination: '/fretcalc',
+                permanent: true,
+            },
+            {
+                source: '/ri',
+                destination: '/ricalc',
+                permanent: true,
+            },
+            {
+                source: '/plq',
+                destination: '/plqsim',
+                permanent: true,
+            },
+            {
+                source: '/tmm',
+                destination: '/tmmsim',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
