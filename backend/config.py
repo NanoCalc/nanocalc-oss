0ecb6f5420c5e4b199d1c2f8e56dbf5e67d2fcb0
OmarMesqq <omarmsqt@gmail.com>
2025-01-06 19:15:28 -0300

    ci: :test_tube: create local upload folder for flask logs during ci

--- backend/config.py
+++ backend/config.py
@@ -3,8 +3,11 @@ import logging
 
 class Config:
     ENV = os.environ.get('ENV', 'prod')
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', '/app/upload')
+
     LOGGING_LEVEL = logging.INFO if ENV == 'dev' else logging.WARN
+    LOG_PATH = os.path.join(UPLOAD_FOLDER, 'backend', 'logs') if ENV == 'dev' else '/app/backend/logs'
+
     LOGGING_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', '/app/upload')
     MAX_CONTENT_LENGTH = 1 * 1024 * 1024  # 1 MB
--- backend/setup_local_env.sh
+++ backend/setup_local_env.sh
@@ -5,3 +5,4 @@ mkdir -p ./nanocalc_uploads/fretcalc/result
 mkdir -p ./nanocalc_uploads/ricalc/result
 mkdir -p ./nanocalc_uploads/plqsim/result
 mkdir -p ./nanocalc_uploads/tmmsim/result
+mkdir -p ./nanocalc_uploads/backend/logs
