e64e8af501d83cae030f67317f5c6c5a2003e586
OmarMesqq <omarmsqt@gmail.com>
2025-01-06 18:59:45 -0300

    test: :test_tube: update copy mock script

--- backend/copy_mocks.sh
+++ backend/copy_mocks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp mocks/fret_calc_mock.py fret_calc.py
-cp mocks/plq_sim_mock.py plq_sim.py
-cp mocks/ri_calc_mock.py ri_calc.py
-cp mocks/tmm_sim_mock.py tmm_sim.py 
+cp mocks/fret_calc_mock.py fretcalc_core.py
+cp mocks/plq_sim_mock.py plqsim_core.py
+cp mocks/ri_calc_mock.py ricalc_core.py
+cp mocks/tmm_sim_mock.py tmmsim_core.py 
