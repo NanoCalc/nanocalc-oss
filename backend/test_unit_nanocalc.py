55907a65eacfc8ed039a4cf620362b278314658e
OmarMesqq <omarmsqt@gmail.com>
2025-03-01 17:11:44 -0300

    test: :test_tube: update generate zip function test for new api

--- backend/test_unit_nanocalc.py
+++ backend/test_unit_nanocalc.py
@@ -1,4 +1,4 @@
-from helper_functions import save_file_with_uuid, generate_zip
+from helper_functions import generate_zip
 import unittest
 import unittest.mock as mock
 import os
@@ -6,30 +6,8 @@ import tempfile
 import zipfile
 
 class TestHelperFunctions(unittest.TestCase):
-
-    @mock.patch('uuid.uuid4')
-    def test_save_file_with_uuid(self, mock_uuid4):
-        mock_uuid4.return_value = "mockUUID"
-        filename_mock = "testfile.csv"
-
-        with tempfile.TemporaryDirectory() as tmpdir:
-            file_mock = mock.MagicMock()
-            file_mock.filename = filename_mock
-
-            finalPath = save_file_with_uuid(tmpdir, file_mock)
-            finalFileName = finalPath.split("/").pop()
-            
-            self.assertEqual(finalFileName, "mockUUID.csv")
-            mock_uuid4.assert_called_once()
-            self.assertTrue(finalPath.startswith(tmpdir))
-            self.assertTrue(finalPath.endswith(".csv"))
-
-            
-
-    @mock.patch('uuid.uuid4')
-    def test_generate_zip(self, mock_uuid4):
-        mock_uuid4.return_value = "mockUUID"
-        webAppName_mock = "randomNanocalcApp"
+    
+    def test_generate_zip(self):
 
         with tempfile.TemporaryDirectory() as sourceDir, tempfile.TemporaryDirectory() as targetDir:
             file_mock1 = mock.MagicMock()
@@ -42,10 +20,10 @@ class TestHelperFunctions(unittest.TestCase):
             with open(os.path.join(sourceDir, "myData.dat"), 'w') as f:
                 f.write("Data | in | columns")
 
-            finalZipFilePath = generate_zip(sourceDir, webAppName_mock, targetDir)
+            finalZipFilePath = generate_zip(sourceDir, targetDir)
             finalZipFileName = finalZipFilePath.split("/").pop()
 
-            self.assertEqual(finalZipFileName, "mockUUID" + "-generated-data.zip")
+            self.assertEqual(finalZipFileName, "generated-data.zip")
 
             with zipfile.ZipFile(finalZipFilePath, 'r') as zip_ref:
                 self.assertIn("myPicture.png", zip_ref.namelist()) 
@@ -58,8 +36,6 @@ class TestHelperFunctions(unittest.TestCase):
                 with zip_ref.open("myData.dat") as file:
                     content = file.read().decode('utf-8')
                     self.assertEqual(content, "Data | in | columns")
-
-
 
 
 if __name__ == '__main__':
